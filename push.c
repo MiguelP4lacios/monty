ec08b85e3d8734ca8f5776d757215439dda549ea
angellovc <vc.angel@outlook.com>
2020-05-13 22:53:48 +0000

    Fix: exit status was fixed and errors was added

--- push.c
+++ push.c
@@ -8,12 +8,17 @@ void push(stack_t **stack, __attribute__((unused))unsigned int last_line)
 {
 	stack_t *new, *tmp;
 
+	if (line[1][0] < 48 || line[1][0] > 57)
+	{
+		fprintf(stderr, "L%i: usage: push integer\n", last_line);
+		exit(EXIT_FAILURE);
+	}
 	tmp = *stack;
 	new = malloc(sizeof(stack_t));
 	if (new == '\0')
 	{
-		fprintf(stderr, "Error: malloc failed");
-		EXIT_FAILURE;
+		fprintf(stderr, "Error: malloc failed\n");
+		exit(EXIT_FAILURE);
 	}
 	new->next = '\0';
 	new->n = atoi(line[1]);
