942fe8f7947068646883268bcbe4bcd0ddf55a5d
angellovc <vc.angel@outlook.com>
2020-05-14 22:30:11 +0000

    Rename: rot1 was renamed to rotl

--- rotr.c
+++ rotr.c
@@ -1,10 +1,10 @@
 #include "monty.h"
 /**
- *rot1 - swap the first and last values of the stack
+ *rotr - swap the first and last values of the stack
  *@stack: stack of values
  *@line_number: line of instruction
  */
-void rot1(stack_t **stack, unsigned int __attribute__((unused))line_number)
+void rotr(stack_t **stack, unsigned int __attribute__((unused))line_number)
 {
 	stack_t *top = '\0', *bottom = '\0';
 
