ff24ccb3e3f62cca3a0b00d19f2735076d52f3f4
angellovc <vc.angel@outlook.com>
2020-05-14 23:24:41 +0000

    Fix: empty stack bug was fixed in rotl.c and rotr.c

--- rotl.c
+++ rotl.c
@@ -8,6 +8,8 @@ void rotl(stack_t **stack, unsigned int __attribute__((unused))line_number)
 {
 	stack_t *top, *bottom;
 
+	if (stack == '\0' || *stack == '\0')
+		return;
 	top = *stack, bottom = *stack;
 	while (bottom->next != '\0')
 		bottom = bottom->next;
